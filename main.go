9e8c7d25b33af049a6a0c06fdd8f58625c997dbf
Dylan Ashby <dylanashby@Dylans-MBP.lan>
2023-04-09 15:21:53 -0400

    hey it runs

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"log"
 )
 
 const (
@@ -11,112 +13,77 @@ const (
 	screenHeight = 480
 )
 
-type Game struct {
-	junoImage *ebiten.Image
+var g *game
+
+type game struct {
 	oniImage  *ebiten.Image
-	junoPos   ebiten.GeoM
+	junoImage *ebiten.Image
 	oniPos    ebiten.GeoM
-	speed     float64
+	junoPos   ebiten.GeoM
 }
 
-func (g *Game) Update(screen *ebiten.Image) error {
+func (g *game) Update() error {
+	// Update Juno position
+	junoSpeed := 4.0
+	var junoPos ebiten.GeoM
+	junoPos.Concat(g.junoPos)
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		g.junoPos.Translate(-g.speed, 0)
+		junoPos.Translate(-junoSpeed, 0)
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		g.junoPos.Translate(g.speed, 0)
+		junoPos.Translate(junoSpeed, 0)
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		g.junoPos.Translate(0, -g.speed)
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		g.junoPos.Translate(0, g.speed)
-	}
+	g.junoPos = junoPos
 
-	if ebiten.IsKeyPressed(ebiten.KeyJ) {
-		g.oniPos.Translate(-g.speed, 0)
+	// Update Oni position
+	oniSpeed := 2.0
+	var oniPos ebiten.GeoM
+	oniPos.Concat(g.oniPos)
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		oniPos.Translate(-oniSpeed, 0)
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyL) {
-		g.oniPos.Translate(g.speed, 0)
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		oniPos.Translate(oniSpeed, 0)
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyI) {
-		g.oniPos.Translate(0, -g.speed)
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyK) {
-		g.oniPos.Translate(0, g.speed)
-	}
+	g.oniPos = oniPos
 
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	var junoImageOpts ebiten.DrawImageOptions
-	junoImageOpts.GeoM = g.junoPos
-	screen.DrawImage(g.junoImage, &junoImageOpts)
+func (g *game) Draw(screen *ebiten.Image) {
+	screen.DrawImage(g.oniImage, &ebiten.DrawImageOptions{
+		GeoM: g.oniPos,
+	})
+	screen.DrawImage(g.junoImage, &ebiten.DrawImageOptions{
+		GeoM: g.junoPos,
+	})
 
-	var oniImageOpts ebiten.DrawImageOptions
-	oniImageOpts.GeoM = g.oniPos
-	screen.DrawImage(g.oniImage, &oniImageOpts)
-
-	if g.junoPos.X < g.oniPos.X {
-		var junoImageOpts ebiten.DrawImageOptions
-		junoImageOpts.GeoM.Concat(&g.junoPos)
-		junoImageOpts.GeoM.Scale(-1, 1)
-		junoImageOpts.GeoM.Translate(64, 0)
-		screen.DrawImage(g.junoImage, &junoImageOpts)
-	} else {
-		var junoImageOpts ebiten.DrawImageOptions
-		junoImageOpts.GeoM.Concat(&g.junoPos)
-		screen.DrawImage(g.junoImage, &junoImageOpts)
-	}
-
-	if g.oniPos.X < g.junoPos.X {
-		var oniImageOpts ebiten.DrawImageOptions
-		oniImageOpts.GeoM.Concat(&g.oniPos)
-		oniImageOpts.GeoM.Scale(-1, 1)
-		oniImageOpts.GeoM.Translate(64, 0)
-		screen.DrawImage(g.oniImage, &oniImageOpts)
-	} else {
-		var oniImageOpts ebiten.DrawImageOptions
-		oniImageOpts.GeoM.Concat(&g.oniPos)
-		screen.DrawImage(g.oniImage, &oniImageOpts)
-	}
+	fps := ebiten.CurrentFPS()
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f", fps))
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+func (g *game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
 func main() {
-	// create game object
-	g := &Game{}
-
-	// initialize ebiten
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Juno")
-
-	// load images
-	junoImage, _, err := ebitenutil.NewImageFromFile("assets/juno.png", ebiten.FilterDefault)
+	oniImage, _, err := ebitenutil.NewImageFromFile("assets/oni.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	oniImage, _, err := ebitenutil.NewImageFromFile("assets/oni.png", ebiten.FilterDefault)
+	junoImage, _, err := ebitenutil.NewImageFromFile("assets/juno.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// set image options
-	var junoImageOpts ebiten.DrawImageOptions
-	var oniImageOpts ebiten.DrawImageOptions
+	g := &game{
+		oniImage:  oniImage,
+		junoImage: junoImage,
+	}
 
-	// set positions
-	g.junoPos = ebiten.GeoM{}
-	g.oniPos = ebiten.GeoM{}
-	g.oniPos.Translate(300, 0)
-
-	// start game
-	if err := ebiten.RunGame(g); err != nil {
-		log.Fatal(err)
+	err = ebiten.RunGame(g)
+	if err != nil {
+		return
 	}
 }
