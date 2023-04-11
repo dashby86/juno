2b5b18a901804a6f917acab5b37cd69fc393b0d9
dashby <dylan.ashby@rebuyengine.com>
2023-04-11 11:23:43 -0400

    added more

--- main.go
+++ main.go
@@ -14,11 +14,6 @@ const (
 )
 
 func main() {
-	oniImage, _, err := ebitenutil.NewImageFromFile("assets/oni.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	junoImage, _, err := ebitenutil.NewImageFromFile("assets/juno.png")
 	if err != nil {
 		log.Fatal(err)
@@ -27,23 +22,7 @@ func main() {
 	junoPos := ebiten.GeoM{}
 	junoPos.Translate(screenWidth/2, float64(screenHeight-junoImage.Bounds().Max.Y))
 
-	// Create and configure camera
-	cam := &structs.Camera{
-		X:       screenWidth / 2,
-		Y:       screenHeight / 2,
-		Speed:   4.0,
-		Zoom:    1.0,
-		MinZoom: 0.5,
-		MaxZoom: 2.0,
-	}
-
-	// Create and configure background
-	backgroundImage, _, err := ebitenutil.NewImageFromFile("assets/background.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	g, err := structs.NewGame(oniImage, junoImage, "assets/background.png", screenWidth, screenHeight)
+	g, err := structs.NewGame("assets/oni.png", "assets/juno.png", "assets/background.png", screenWidth, screenHeight)
 	if err != nil {
 		log.Fatal(err)
 	}
